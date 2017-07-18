46a08a4fd1cb54d91128fecb118fa0b7a2027b00
Luke Woodward <luke.wdwrd@gmail.com>
2017-07-18 10:24:41 -0500

    adjusted UI to fit the flow of the contract

--- blockchain/contracts/Transfer.sol
+++ blockchain/contracts/Transfer.sol
@@ -2,14 +2,57 @@ pragma solidity ^0.4.11;
 
 contract Transfer {
 
-    address seller;
-    address buyer;
-    uint asset;
+    address public seller;
+    address public buyer;
+    uint public asset;
 
     // establishes stages on state
     enum State { Created, Confirmed, Disabled }
     // saves instantiated state into a variable
-    State state;
+    State public state;
+
+
+    event PaymentConfirmed(address buyer, uint datePurchased);
+    event GoodTransfered(address buyer, address seller, uint assetPrice, uint dateTransfered);
+    event Refund(address buyer, address seller, uint refundAmount, uint dateRefunded);
+
+    function Transfer() correctValue(msg.value % 2 == 0) payable {
+        // contains the address of the one who calls the function
+        seller = msg.sender;
+        // has to send 2x as a deposit so the asset is adjusted
+        asset = msg.value / 2;
+    }
+
+    // can only be called in Created State and if the value is double
+    function confirmPurchase() currentState(State.Created) correctValue(msg.value == (2 * asset)) payable {
+        buyer = msg.sender;
+        //changes state to Confirmed
+        state = State.Confirmed;
+        // event
+        PaymentConfirmed(buyer, now);
+    }
+
+    // can only be called in Confirmed State by the buyer
+    function confirmRecieved() currentState(State.Confirmed) onlyBuyer {
+        buyer.transfer(asset);
+        // anything left after the the buyer is confirmed will go back to the seller
+        seller.transfer(this.balance);
+        state = State.Disabled;
+        // event
+        GoodTransfered(buyer, seller, asset, now);
+    }
+
+    // can only be called in Confirmed State by the seller
+    function refundBuyer() currentState(State.Confirmed) onlySeller {
+        buyer.transfer(2 * asset);
+        seller.transfer(this.balance);
+        state = State.Disabled;
+        // event
+        Refund(buyer, seller, 2 * asset, now);
+    }
+
+
+    /** MODIFIER METHODS **/
 
     modifier correctValue(bool c) {
         require(c);
@@ -26,45 +69,6 @@ contract Transfer {
     modifier onlySeller() {
         require(msg.sender == seller);
         _;
-    }
-
-    event TransferConfirmed(address buyer, uint datePurchased);
-    event TransferReceived(address buyer, address seller, uint assetPrice, uint dateTransfered);
-    event Refund(address buyer, address seller, uint refundAmount, uint dateRefunded);
-
-    function Transfer() correctValue(msg.value % 2 == 0) payable {
-        // contains the address of the one who calls the function
-        seller = msg.sender;
-        // has to send 2x as a deposit so the asset is adjusted
-        asset = msg.value / 2;
-    }
-
-    // can only be called in Created State and if the value is double
-    function confirmPurchase() currentState(State.Created)  {
-        buyer = msg.sender;
-        //changes state to Confirmed
-        state = State.Confirmed;
-        // event
-        TransferConfirmed(buyer, now);
-    }
-
-    // can only be called in Confirmed State by the buyer
-    function confirmRecieved() currentState(State.Confirmed) onlyBuyer {
-        buyer.transfer(asset);
-        // anything left after the the buyer is confirmed will go back to the seller
-        seller.transfer(this.balance);
-        state = State.Disabled;
-        // event
-        TransferReceived(buyer, seller, asset, now);
-    }
-
-    // // can only be called in Confirmed State by the seller
-    function refundBuyer() currentState(State.Confirmed) onlySeller {
-        buyer.transfer(2 * asset);
-        seller.transfer(this.balance);
-        state = State.Disabled;
-        // event
-        Refund(buyer, seller, 2 * asset, now);
     }
 
 }
--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import './App.css';
 import Web3 from 'web3';
 
 const ETHEREUM_PROVIDER = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-const TRANSFER_ABI = [{"constant":false,"inputs":[],"name":"confirmRecieved","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"confirmPurchase","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"refundBuyer","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":true,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"datePurchased","type":"uint256"}],"name":"TransferConfirmed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"seller","type":"address"},{"indexed":false,"name":"assetPrice","type":"uint256"},{"indexed":false,"name":"dateTransfered","type":"uint256"}],"name":"TransferReceived","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"seller","type":"address"},{"indexed":false,"name":"refundAmount","type":"uint256"},{"indexed":false,"name":"dateRefunded","type":"uint256"}],"name":"Refund","type":"event"}];
-const TRANSFER_ADDRESS = '0x5de37645b4b96096782e769daf283346bdc0e564';
+const TRANSFER_ADDRESS = '0xfd046a97d595b184a42dce647d088dbacb8038b6';
+const TRANSFER_ABI = [{"constant":true,"inputs":[],"name":"seller","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"confirmRecieved","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"asset","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"buyer","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"state","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"confirmPurchase","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"refundBuyer","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":true,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"datePurchased","type":"uint256"}],"name":"PaymentConfirmed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"seller","type":"address"},{"indexed":false,"name":"assetPrice","type":"uint256"},{"indexed":false,"name":"dateTransfered","type":"uint256"}],"name":"GoodTransfered","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"seller","type":"address"},{"indexed":false,"name":"refundAmount","type":"uint256"},{"indexed":false,"name":"dateRefunded","type":"uint256"}],"name":"Refund","type":"event"}]
 const TRANSFER_CONTRACT = ETHEREUM_PROVIDER.eth.contract(TRANSFER_ABI).at(TRANSFER_ADDRESS);
+const COINBASE = ETHEREUM_PROVIDER.eth.coinbase;
+const SELLER = ETHEREUM_PROVIDER.eth.accounts[1];
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-
+      buyerBalance: null,
+      sellerBalance: null
     }
   }
 
   componentDidMount(){
+    this.setState({
+      buyerBalance: ETHEREUM_PROVIDER.fromWei(ETHEREUM_PROVIDER.eth.getBalance(COINBASE).toString(10)),
+      sellerBalance: ETHEREUM_PROVIDER.fromWei(ETHEREUM_PROVIDER.eth.getBalance(SELLER).toString(10))
+    })
+
   }
+
+  _purchase() {
+    TRANSFER_CONTRACT.confirmPurchase({to: SELLER, from: COINBASE, gas: 210000}, (err, res) => {
+      console.log(err, res);
+    })
+  }
+
+
 
   render() {
 
@@ -26,14 +42,33 @@ class App extends Component {
       <div className="App">
         <h1 className="banner">Smart Escrow</h1>
         <div className="content">
+
           <div className="section">
-            <img src={"http://amplifiii.com/flatso/wp-content/uploads/2013/09/flat-faces-icons-circle-man-4_256x256x32.png"} alt={"seller"} className="traders" />
-            <button />
+            <div className="userHeading">
+              <img src={"https://cloudposse.com/wp-content/uploads/sites/29/2016/04/icon-mentors-e1459485567739.png"} alt={"buyer" } className="userImg" />
+              <div className="userInfo">
+                <h1>buyer</h1>
+                <h3>{parseFloat(this.state.buyerBalance).toFixed(2)}</h3>
+              </div>
+            </div>
           </div>
+
           <div className="section">
-            <img src={"https://cloudposse.com/wp-content/uploads/sites/29/2016/04/icon-mentors-e1459485567739.png"} alt={"buyer" } className="traders" />
-            <img src={"https://cloudposse.com/wp-content/uploads/sites/29/2016/04/icon-mentors-e1459485567739.png"} alt={"buyer" } className="traders" />
+
           </div>
+
+          <div className="section">
+            <div className="userHeading">
+              <img src={"http://amplifiii.com/flatso/wp-content/uploads/2013/09/flat-faces-icons-circle-man-4_256x256x32.png"} alt={"seller"} className="userImg" />
+              <div className="userInfo">
+                <h1>seller</h1>
+                <h3>{parseFloat(this.state.sellerBalance).toFixed(2)}</h3>
+              </div>
+            </div>
+            <img src={"http://www.freeiconspng.com/uploads/3d-printer-interface-symbol-2.png"} alt={"seller"} className="item" />
+            <button className="purchaseBtn" onClick={()=>{this._purchase()}}>Purchase</button>
+          </div>
+
         </div>
       </div>
     );
